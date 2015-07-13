a7f64b89971667a5cc2d22104af219528c122436
Patrick Nielsen <ptnielsen55@gmail.com>
2015-07-13 15:09:08 -0400

    Add Show to Board

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,6 +1,12 @@
 class BoardsController < ApplicationController
 
   def index
+    @boards = Board.all
+  end
+
+  def show
+    @board = Board.find_by_id params[:id]
+    @comments = Comment.where(post_id: params[:id])
   end
 
 end
